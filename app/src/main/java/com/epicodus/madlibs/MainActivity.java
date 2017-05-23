998c2c837c3586447093c4fa590c8ea678b43fb7
Mark Fisher and Xi Xia <mark@>
2017-05-23 16:44:01 -0700

    radio button

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,6 +23,7 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.makeStoryButton) Button mMakeStoryButton;
     @Bind(R.id.baseGridView) GridView gridView;
+    @Bind(R.id.rg) RadioGroup radioGroup;
 
 
     @Override
@@ -42,15 +45,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ArrayList<String> allFields = new ArrayList<>();
             Resources res = getResources();
             String [] userPrompts = res.getStringArray(R.array.user_input);
-            Log.d("userPrompts?", Arrays.toString(userPrompts));
             for(int i = 0; i<userPrompts.length; i++){
                 LinearLayout listLayout = (LinearLayout) gridView.getChildAt(i);
                 EditText editField = (EditText) listLayout.getChildAt(0);
                 allFields.add(editField.getText().toString());
             }
+            int selectedRadioButtonID = radioGroup.getCheckedRadioButtonId();
+
+            if(selectedRadioButtonID !=-1){
+                RadioButton selectedRadioButton = (RadioButton) findViewById(selectedRadioButtonID);
+                String selectedRadioButtonText = selectedRadioButton.getText().toString();
+                Log.d("Radio text", selectedRadioButtonText);
+            }
+
             Intent intent = new Intent(MainActivity.this, StoryActivity.class);
             intent.putStringArrayListExtra("allFields", allFields);
             startActivity(intent);
+        }
+    }
+
+    public void onRadioButtonClicked(View view) {
+        //Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_yes:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.radio_no:
+                if (checked)
+                    // Ninjas rule
+                    break;
         }
     }
 }

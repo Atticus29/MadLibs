2b076083a2b81d8351c19239a2742641062a02cb
Mark Fisher and Xi Xia <mark@>
2017-05-23 10:40:24 -0700

    successfully pass data to the story activity

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -5,12 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.makeStoryButton) Button mMakeStoryButton;
+    @Bind(R.id.personNameInput) EditText mPersonNameInput;
+    @Bind(R.id.locationInput) EditText mLocationInput;
+    @Bind(R.id.verbInput) EditText mVerbInput;
+    @Bind(R.id.nounInput) EditText mNounInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +26,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v){
-        if(v == mMakeStoryButton){
+        if(v == mMakeStoryButton) {
+            String properNoun = mPersonNameInput.getText().toString();
+            String location = mLocationInput.getText().toString();
+            String verb = mVerbInput.getText().toString();
+            String noun = mNounInput.getText().toString();
+            Bundle extras = new Bundle();
+            extras.putString("properNoun", properNoun);
+            extras.putString("location", location);
+            extras.putString("verb", verb);
+            extras.putString("noun", noun);
             Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+            intent.putExtras(extras);
             startActivity(intent);
         }
     }
--- app/src/main/java/com/epicodus/madlibs/StoryActivity.java
+++ app/src/main/java/com/epicodus/madlibs/StoryActivity.java
@@ -1,13 +1,29 @@
 package com.epicodus.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class StoryActivity extends AppCompatActivity {
-
+    @Bind(R.id.textView) TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+        ButterKnife.bind(this);
+        Bundle extras = getIntent().getExtras();
+        String properNoun = extras.getString("properNoun");
+        String location = extras.getString("location");
+        String verb = extras.getString("verb");
+        String noun = extras.getString("noun");
+        String story = String.format("One day, %s went to %s. They %s all over %s.", properNoun, location, verb, noun);
+        Log.d("story", story);
+        mTextView.setText(story);
     }
 }
